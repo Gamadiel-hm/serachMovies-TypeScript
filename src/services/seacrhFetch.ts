f8e9b12555161442afd89a4057e6f77e068288a4
Gamadiel <gamadiel-hm@hotmail.com>
2023-04-28 13:01:33 -0600

    Conclusion de App

--- src/services/seacrhFetch.ts
+++ src/services/seacrhFetch.ts
@@ -1,7 +1,7 @@
 const API_URL = 'https://www.omdbapi.com/?apikey=34471800&s=';
 
 function searchFetch() {
-  const fetching = (search: string) => fetch(API_URL + (search ?? 'marvel'));
+  const fetching = (search: string) => fetch(API_URL + search);
 
   return { fetching };
 }
